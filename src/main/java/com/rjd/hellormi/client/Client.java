0f5a4d825b2069a09df24bf949010b8f53d94b51
Remko de Jong <aggenebbisj@gmail.com>
2015-02-08 21:18:12 +0100

    first commit

---
+++ src/main/java/com/rjd/hellormi/client/Client.java
@@ -0,0 +1,14 @@
+package com.rjd.hellormi.client;
+
+import com.rjd.hellormi.HelloService;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class Client {
+    public static void main( String[] args ) throws NotBoundException, MalformedURLException, RemoteException {
+        HelloService service = (HelloService) Naming.lookup("rmi://localhost:5099/hello");
+        System.out.println("--- " + service.echo("Hello Server"));
+    }
+}
---
+++ src/main/java/com/rjd/hellormi/server/HelloServant.java
@@ -0,0 +1,17 @@
+package com.rjd.hellormi.server;
+
+import com.rjd.hellormi.HelloService;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HelloServant extends UnicastRemoteObject implements HelloService {
+
+    public HelloServant() throws RemoteException {
+        super();
+    }
+    
+    public String echo(String input) throws RemoteException {
+        return "From server: " + input;
+    }
+    
+}
